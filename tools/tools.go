95494a546c2f524264cfd25ba38f1729a32c436b
Bharath B <bhb@redhat.com>
2025-12-04 11:55:53 +0530

    ESO-287: Updates and vendors the pkgs required by golangci-lint/v2
    Signed-off-by: Bharath B <bhb@redhat.com>

--- tools/tools.go
+++ tools/tools.go
@@ -16,7 +16,7 @@ import (
 	_ "golang.org/x/vuln/cmd/govulncheck"
 
 	// prow-ci
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/golangci/golangci-lint/v2/cmd/golangci-lint"
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kustomize/kustomize/v5"
